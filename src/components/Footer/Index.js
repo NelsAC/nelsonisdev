21e4c1390d2f7a734354d2cc519a3f442508a586
Nelson Alarcon Cahuana <nelsonelvis.ac@gmail.com>
2021-10-24 10:40:14 -0500

    feat: add footer section

---
+++ src/components/Footer/Index.js
@@ -0,0 +1,13 @@
+import { Socials } from '../Socials/Index'; 
+
+import {
+    Container,
+} from './footer.styles';
+
+export const Footer = () => {
+    return (
+        <Container>
+            <Socials />
+        </Container>
+    )
+}
