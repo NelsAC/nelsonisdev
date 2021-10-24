0dc298cf1c50e73f017022d876fcca961cac6ee4
Nelson Alarcon Cahuana <nelsonelvis.ac@gmail.com>
2021-10-23 23:42:54 -0500

    feat: add menu icon

---
+++ src/assets/Icons/Menu.js
@@ -0,0 +1,13 @@
+export function Menu(props) {
+    return (
+      <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 48 1"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path d="M0 0h48v1H0z" fill="#063855" fillRule="evenodd" />
+      </svg>
+    )
+  }
